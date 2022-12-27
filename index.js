858a93c190370531d480720266d9f4f76e51e0db
Владислав Маковский <alectorkinga@gmail.com>
2022-12-27 04:23:21 +0200

    final

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 var app = express();
-
+const multer = require("multer");
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(process.cwd() + "/public"));
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
+// *====================================================================
+app.post("/api/fileanalyse", multer().single("upfile"), (req, res) => {
+  let fileObj = req.file;
 
-
-
-
+  res.json({
+    name: fileObj.originalname,
+    type: fileObj.mimetype,
+    size: fileObj.size,
+  });
+});
+// *====================================================================
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
